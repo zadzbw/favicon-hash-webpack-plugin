b9b9b58522bb646a2446ad43e37c395dceeca7de
zadzbw <609842385@qq.com>
2018-02-07 18:03:30 +0800

    add source

---
+++ README.md
@@ -0,0 +1,62 @@
+# favicon-hash-webpack-plugin
+
+This is an extension plugin for [html-webpack-plugin](https://github.com/ampedandwired/html-webpack-plugin) - it can add hash to favicon file, just like `favicon.[hash].ico`, the hash algorithm is md5 now, and use [blueimp-md5](https://github.com/blueimp/JavaScript-MD5) to calculate hash.
+
+## Installation
+
+### Notice
+
+> * it requires node **v6** or higher
+> * it requires [webpack](https://webpack.js.org/) **v3** 
+> * it requires that you're using [html-webpack-plugin](https://github.com/ampedandwired/html-webpack-plugin) in your project
+
+### Install
+
+You can install this plugin with npm:
+
+```bash
+npm install favicon-hash-webpack-plugin --save-dev
+```
+
+or you can use yarn:
+
+```bash
+yarn install favicon-hash-webpack-plugin -D
+```
+
+## Usage
+
+First, require the plugin in your webpack config:
+
+```js
+const FaviconHashPlugin = require('favicon-hash-webpack-plugin');
+```
+
+finally, set favicon in `html-webpack-plugin` and add the plugin in `plugins` options after `html-webpack-plugin`
+
+```js
+...
+plugins: [
+  new HtmlWebpackPlugin({
+    ...
+  	favicon: 'path-to-favicon/favicon.ico', // favicon path
+  	...
+  }),
+  new FaviconHashPlugin({}),
+],
+...
+```
+
+and the following icon link will be injected into `<head>`:
+
+```html
+<link rel="shortcut icon" href="your-public-path/favicon.e513b5416b8dc82513c174d5ccde5c21.ico">
+```
+
+## Options
+
+This plugin only has the default options now.
+
+## License
+
+[MIT License](http://www.opensource.org/licenses/mit-license.php)
---
+++ index.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const fs = require('fs');
+const md5 = require('blueimp-md5');
+
+const removeOriginalFavicon = (compilation, faviconPath) => {
+  const basename = path.basename(faviconPath); // original faviconName
+  // remove
+  compilation.fileDependencies = compilation.fileDependencies.filter(name => name !== basename);
+  delete compilation.assets[basename];
+};
+
+// waiting for add other options
+const defaultOptions = {};
+
+class FaviconHashPlugin {
+  constructor(options = {}) {
+    this.options = Object.assign({}, defaultOptions, options);
+  }
+
+  apply(compiler) {
+    compiler.plugin('compilation', compilation => {
+      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, cb) => {
+        const { plugin } = htmlPluginData; // HtmlWebpackPlugin instance
+        const faviconPath = path.resolve(compilation.compiler.context, plugin.options.favicon); // logical path
+        const ext = path.extname(faviconPath);
+        let publicPath = compilation.mainTemplate.getPublicPath({ hash: compilation.hash }) || '';
+        if (publicPath && publicPath.substr(-1) !== '/') {
+          publicPath += '/';
+        }
+        try {
+          const source = fs.readFileSync(faviconPath);
+          const stat = fs.statSync(faviconPath);
+          const hash = md5(source, 'utf-8');
+          const faviconName = `${path.basename(faviconPath, ext)}.${hash}${ext}`;
+          removeOriginalFavicon(compilation, faviconPath);
+          compilation.fileDependencies.push(faviconName); // add new favicon
+          compilation.assets[faviconName] = {
+            source: function () {
+              return source;
+            },
+            size: function () {
+              return stat.size;
+            }
+          };
+          htmlPluginData.assets.favicon = publicPath + faviconName;
+          cb(null, htmlPluginData);
+        } catch (err) {
+          throw new Error('FaviconHashPlugin: could not load file ' + faviconPath)
+        }
+      });
+    });
+  }
+}
+
+module.exports = FaviconHashPlugin;
